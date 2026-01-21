bc93eac1a9660d2c28e90fff56a07196bd8ccb20
Astha Shukla <asthashukla160428@gmail.com>
2026-01-21 19:15:00 +0530

    removed quick access

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -30,7 +30,7 @@ export default function App() {
       <VolunteerForm />
       <ContactSection />
       <Footer />
-      <QuickAccessSidebar />
+      {/*<QuickAccessSidebar />*/}
       <FloatingContactButton />
     </div>
   );
